1b97c663cc3f460ad5b947cd7f6c7f961cbf3b33
高星 <gaoxing@gaos-macbook-pro.local>
2018-06-05 19:59:26 +0800

    Read the archive into memory.

--- AddressBook/main.m
+++ AddressBook/main.m
@@ -9,46 +9,11 @@
 #import "AddressBook.h"
 
 int main(int argc, const char * argv[]) {
+    AddressBook *myBook;
     @autoreleasepool {
-        NSString *aName = @"Julia Kochan";
-        NSString *aEmail = @"jewls337@axlc.com";
-        NSString *bName = @"Tony Iannino";
-        NSString *bEmail = @"jtony.iannino@techfitness.com";
-        NSString *cName = @"Stephen Kochan";
-        NSString *cEmail = @"steve@classroomM.com";
-        NSString *dName = @"Jamie Baker";
-        NSString *dEmail = @"jbaker@classroomM.com";
+        myBook = [NSKeyedUnarchiver unarchiveObjectWithFile:@"addrbook.arch"];
         
-        AddressCard *card1 = [[AddressCard alloc] init];
-        AddressCard *card2 = [[AddressCard alloc] init];
-        AddressCard *card3 = [[AddressCard alloc] init];
-        AddressCard *card4 = [[AddressCard alloc] init];
-        
-        
-        // Set up a new address book
-        
-        AddressBook *myBook = [AddressBook alloc];
-        
-        // Now set up four address cards
-        
-        [card1 setName: aName andEmail: aEmail];
-        [card2 setName: bName andEmail: bEmail];
-        [card3 setName: cName andEmail: cEmail];
-        [card4 setName: dName andEmail: dEmail];
-        
-        myBook = [myBook initWithName:@"Linda's Address Book"];
-        
-        // Add the cards to the address book
-        
-        [myBook addCard: card1];
-        [myBook addCard: card2];
-        [myBook addCard: card3];
-        [myBook addCard: card4];
-        
-        [myBook sort];
-        
-        if ([NSKeyedArchiver archiveRootObject:myBook toFile:@"addrbook.arch"] == NO)
-            NSLog(@"archiving failed");
+        [myBook list];
     }
     return 0;
 }
